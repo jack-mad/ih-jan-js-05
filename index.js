ef2edd451ca9310c311659ab65c8f83fdf044f89
Joaquin Velazquez <joaquin.ve@outlook.com>
2022-01-12 12:23:22 -0600

    agregar propiedades y ciclos

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ console.log(olympicRecords["athleticsSwimming200Men"]);
 
 console.log(olympicRecords["swimming 200 Women"]);
 
+// B. AGREGAR PROPIEDADES
+
 const refresco = {
     nombre: "Coca cola",
     color: "Oscuro",
@@ -40,3 +42,42 @@ refresco.envase = "Plastico";
 refresco["competidor"] = "pepsi"
 
 console.log("Objeto de refresco", refresco)
+
+//C. Propiedades y ciclos
+//Object.keys()
+//Permite convertir las propiedades de un objeto en elementos de un arreglo 
+
+const arrRefresco = Object.keys(refresco);
+
+console.log(arrRefresco);
+
+//for ... in
+//for( variable key in objeto)
+for(let key in refresco){
+    console.log(`La propiedad es ${key}`)
+
+    if( key === "competidor") console.log("Encontré la prop competidor")
+}
+
+
+
+
+//object.values(), convierte los valores de un objeto en elementos de un arreglo
+
+const arrRefrescoVal = Object.values(refresco);
+console.log(arrRefrescoVal);
+
+// for ... in
+
+// for (VARIABLE KEY in OBJECT)
+for (let key in refresco ) {
+	// console.log(`La propiedad es: ${key}`)
+
+	// if(key === "competidor") console.log("Encontré la prop competidor")
+
+	console.log(`La propiedad es ${key} y su valor es ${refresco[key]}`)
+
+
+}
+
+//Arreglos con objetos
