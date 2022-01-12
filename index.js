bb503520359aa44c466427242141b32f6d60cf69
Joaquin Velazquez <joaquin.ve@outlook.com>
2022-01-12 11:55:20 -0600

    agregar objetos

--- index.js
+++ index.js
@@ -13,4 +13,30 @@ console.log(mexico.comida);
 //bracket notation para acceder a una propiedad de un objeto
 console.log(mexico["comida"]);
 
+const olympicRecords = {
+    athletics100Men : "Justin Gatlin",
+    athleticsLongJumpMen : "Mike Power",
+    athleticsSwimming200Men : "Michael Phelps",
+    "swimming 200 Women": "Ariarne Titmus" // propiedades con espacios, caracteres especiales, 
+};
 
+//dot notation para acceder a una propiedad de un objeto
+console.log(olympicRecords.athleticsSwimming200Men);
+
+//bracket notation para acceder a una propiedad de un objeto
+console.log(olympicRecords["athleticsSwimming200Men"]);
+
+console.log(olympicRecords["swimming 200 Women"]);
+
+const refresco = {
+    nombre: "Coca cola",
+    color: "Oscuro",
+    precio: 12,
+    tamanos: [600, 1000]
+}
+
+//agregar valores
+refresco.envase = "Plastico";
+refresco["competidor"] = "pepsi"
+
+console.log("Objeto de refresco", refresco)
